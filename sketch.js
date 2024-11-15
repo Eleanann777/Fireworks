25223b8dfbd4ef628eadee0ba21f0ee771ec3837
Eleanann777 <elenaann2009@icloud.com>
2024-11-15 12:06:58 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,9 @@
 const fireworks = [];
 let gravity;
 var isPaused = false;
+const craters = [];
+const building = [];
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   colorMode(HSB);
@@ -11,13 +14,16 @@ function setup() {
 }
 
 function draw() {
+ 
+  drawBackground(100);
+  
   if (isPaused) {
     push();
     textSize(100);
     textAlign(CENTER, CENTER);
     text("PAUSED", width/2, height/2);
     pop();
- } else {
+  } else {
     colorMode(RGB);
     background(0, 0, 0, 25);
     
@@ -33,8 +39,78 @@ function draw() {
         fireworks.splice(i, 1);
       }
     }
+  }
 }
+function drawBackground(alpha) {
+  push();
+ 
+  colorMode(RGB);
+ 
+  drawMoon(alpha);
+  drawSkyline(alpha);
+  pop();
 }
+
+function drawMoon(alpha) {
+  push();
+  const moonRadius = min(width, height) / 5;
+  const moonX = width / 3;
+  const moonY = height / 2.5;
+
+  noStroke();
+ 
+  fill(200, alpha);
+  circle(moonX, moonY, moonRadius);
+  fill(100, alpha);
+
+  // Clip the craters to the moon
+  drawingContext.save();
+  push();
+  circle(moonX, moonY, moonRadius);
+  pop();  
+  drawingContext.clip();
+
+
+  
+  if (craters.length === 0) {
+    const numCraters = 10;
+    for (let i = 0; i < numCraters; ++i) {
+      let craterWidth = random(moonRadius / 4, moonRadius / 1.5);
+      let craterHeight = random(moonRadius / 4, moonRadius / 1.5);
+      let craterX = moonX + random(-moonRadius, moonRadius) ;
+      let craterY = moonY + random(-moonRadius, moonRadius);
+      craters.push([craterX, craterY, craterWidth, craterHeight]);
+    }
+  }
+
+  
+  craters.forEach((crater) => {
+    ellipse(...crater);  
+  });
+
+  drawingContext.restore();
+  pop();
+}
+
+function drawSkyline(alpha) {
+  push();
+
+  
+  if (buildings.length === 0) {
+
+    
+    let buildingHeight = random(height/30, height/4);
+    let buildingWidth = random(width/20, width/10);
+    let buildingLocation = 0; 
+    buildings.push([buildingLocation, height - buildingHeight, buildingWidth, buildingHeight]);
+  }
+
+  noStroke();
+  fill(100, alpha);
+  buildings.forEach(building => rect(...building));
+  pop();
+}
+
 function keyPressed() {
   console.log("You pressed: " + key);
   if (key === 'p') {
